efc2bbdbd53ad84eb3ddd09e79329a1ae741cbfc
yashwanth <2311cs010648@mallareddyunversity.ac.in>
2025-01-23 22:16:22 +0530

    assignment 2

--- Assignment2.java
+++ Assignment2.java
@@ -1,15 +1,17 @@
 class A2{
     public static void main(String [] args){
-        int arr[] = {101,201,301,401,501};
+        int arr[] = {123,456,789};
+
         for (int i = 0;i<arr.length;i++) {
-            int rev = 0;
+            int Added = 0;
             while(arr[i]>0){
                 int last = arr[i]%10;
-                rev = rev*10+last;
+                Added+=last;
                 arr[i] = arr[i]/10;
             }
-            arr[i] = rev;
+            arr[i] = Added;
         }
+
         for (int i : arr) {
             System.out.print(" "+i);
         }
